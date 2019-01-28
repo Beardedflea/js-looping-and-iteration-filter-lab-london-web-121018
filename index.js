01a8fcbe77c3ebdd2c0aceac1363571f3e8a76ef
github username <github email address>
2019-01-28 18:52:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+
+function findMatching(drivers, string){
+    return drivers.filter(word => word.toUpperCase() === string.toUpperCase());
+}
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter(word => word.startsWith(string));
+}
+
+function matchName(drivers, string){
+    let matchNames= [];
+    for (const key in drivers){
+        if (drivers[key].name == string){
+        matchNames.push(drivers[key]);
+        }
+    }
+    return matchNames
+}
